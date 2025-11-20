d6ce48d58856bba971b292180949712e1fad94ed
Brittany Price <bprice0496@gmail.com>
2025-11-19 22:23:51 -0500

    Added back button

--- src/Components/Pong.jsx
+++ src/Components/Pong.jsx
@@ -1,23 +1,28 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Board from "./Board";
+import PongStyles from "../css/Pong.module.css";
 
 function Pong() {
+    const navigation = useNavigate();
     const [player1, setPlayer1] = useState({
-    size: 18,
-    color: "blue",
+        size: 18,
+        color: "blue",
     });
 
     const [player2, setPlayer2] = useState({
-    size: 18,
-    color: "red",
+        size: 18,
+        color: "red",
     });
 
     const [ball, setBall] = useState({
-    size: 12,
-    color: "white",
+        size: 12,
+        color: "white",
     });
+
     return (
         <>
+          <button className={PongStyles.back} onClick={() => navigation(-1)}>&larr; back</button>
           <Board/>
         </>
     )
