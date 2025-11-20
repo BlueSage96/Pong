9a534c39f6db09c1d0537bf561fe702670fdcaa6
Brittany Price <bprice0496@gmail.com>
2025-11-20 02:52:34 -0500

    Created players and ball

--- src/Components/Pong.jsx
+++ src/Components/Pong.jsx
@@ -5,25 +5,24 @@ import PongStyles from "../css/Pong.module.css";
 
 function Pong() {
     const navigation = useNavigate();
-    const [player1, setPlayer1] = useState({
-        size: 18,
-        color: "blue",
-    });
+    const [player, setPlayer] = useState(0);
+    const [ball, setBall] = useState({w: 4, h:4,color: "white"});
 
-    const [player2, setPlayer2] = useState({
-        size: 18,
-        color: "red",
-    });
-
-    const [ball, setBall] = useState({
-        size: 12,
-        color: "white",
-    });
-
+    /*
+        Update ball and player positions and score using functional state updates
+        Examples: 
+        setBall(prevPos => prevPos + 1);
+        setPlayer(play => play + 1);
+        setScore(s => s + 1);
+    */
     return (
         <>
           <button className={PongStyles.back} onClick={() => navigation(-1)}>&larr; back</button>
           <Board/>
+          {/* use onKeyUp OR onKeyDown! */}
+        <div className={PongStyles.P1}></div>
+        <div className={PongStyles.P2}></div>
+        <div className={PongStyles.Ball}></div>
         </>
     )
 }
