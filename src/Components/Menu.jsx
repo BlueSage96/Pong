1acc37846958a3ffdb28f947f274c37d65069b2a
Brittany Price <bprice0496@gmail.com>
2025-11-19 21:34:33 -0500

    Added background image for title screen

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,15 +1,38 @@
 import MenuStyles from "styled-components";
+import Atari from "../assets/Atari.jpg";
+const Screen = MenuStyles.div`
+    // background-color: black;
+    width: 850px;
+    height: 900px;
+    border: 3px solid white;
+    border-radius: 8px;
+    display: flex;
+    position: absolute;
+    left: 500px;
+    top: 25px;
+    align-items: center;
+    justify-content: center;
+`;
+
 const Title = MenuStyles.h1`
     position: absolute;
     top: 0;
+    left: 350px;
     font-size: 64px;
 `;
 
+const Pic = MenuStyles.img`
+    width: 850px;
+    height: 900px;
+`;
 
 export default function Menu() {
     return (
         <>
-          <Title>Pong</Title>
+          <Screen>
+            <Pic src={Atari}/>
+            <Title>Pong</Title>
+          </Screen>
         </>
     )
 }
